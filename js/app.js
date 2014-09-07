bd6c54e29a8bc30bfcf5f07c3c00b3c7f7de6106
Skrier <alex.lewis79@yahoo.co.uk>
2014-09-07 11:46:20 +0100

    v2

---
+++ js/app.js
@@ -0,0 +1,47 @@
+$(document).ready( function () {
+
+/* Navbar buttons*/
+
+$('#searchButton').on('mousedown', function () { 
+	var location = $(this).find('input[name="search"]').val();
+	getGoogleMaps();
+});
+
+$('#aboutButton').on('mousedown', function(){
+
+
+
+});
+
+/*instagram Api*/
+
+
+
+/*twitter Api*/
+
+
+/*Foursquare Api*/
+
+/*Google Maps Api*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
