fa9219b0cee0c3eb803e186894bc1b6080c881f3
Skrier <alex.lewis79@yahoo.co.uk>
2014-09-19 11:45:09 +0100

    g v38

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready( function () {
     /* Gloabl variables*/
     var lat;
     var long;
+    var id;
 
 	/* Navbar buttons*/
 	function search () {
@@ -9,14 +10,15 @@ $(document).ready( function () {
 	    console.log(address);
 	    iframe(address);
 	    getGeoLocation(address);
-	    
+	    getLocationID(lat,long); 
+	    getLocationMedia(id);  
 	}
 
 	$('#searchButton').on('mousedown', function (event) { 
 		search();
 	});
 
-	$(document).find('input[name="search"]').on('keyup', function (event) {
+	$('#textBox').on('keyup', function (event) {
 		event.stopPropagation();
 		if (event.which === 13) {
 	    search();
@@ -38,7 +40,8 @@ $(document).ready( function () {
 	    	data: params,
 	    	dataType:'jsonp',
 	    	type: 'GET',
-	    	jsonp: 'jsonp'
+	    	jsonp: 'jsonp',
+	    	cache: false
 	    })
 	    .done(function(request){
 	    	console.log(request);
@@ -55,12 +58,50 @@ $(document).ready( function () {
 	function iframe (address) {
 			var iFrameLocation = $('iframe');
 			iFrameLocation.attr('src','https://www.google.com/maps/embed/v1/place?q=' +address+'&key=AIzaSyDex2UEUst7Y46h6_jlf4KDdVmY6ws8jGw');
+	} 
+	/* instagram Api */
+	var getLocationID = function(lat,long) {
+        var params = {
+        	client_id: 247fae46590145138b72d9e2d47d9231,
+        	LAT: lat,
+        	lNG: long,
+        	distance: '500',
+        	callback: 'callbackFunction'
+        };
+        var request =$.ajax({
+        	url: 'https://api.instagram.com/v1/locations/search?',
+            dataType: 'jsonp',
+            cache: false,
+            type: 'GET'
+
+        })
+        .done(function(request){
+        	console.log(request);
+        	console.log(request.data[0].id);
+        })
+        .fail(function (jqXHR, error, errorThrown){
+	    	console.log(error);
+	    });       
+	};
+
+	var getLocationMedia = function (id) {
+		var params = {
+        	client_id: 247fae46590145138b72d9e2d47d9231,  
+        	callback: 'callbackFunction'
+		}
+        var request =$.ajax({
+        	url: 'https://api.instagram.com/v1/locations/'+ id+'/media/recent?',
+            dataType: 'jsonp',
+            cache: false,
+            type: 'GET'
+        })
+        .done(function(request){
+        	console.log(request);
+        	console.log(request.data.images.thumbnail.url);
+        })
+        .fail(function (jqXHR, error, errorThrown){
+	    	console.log(error);
+	    });     
 	}
-	 /* instagram Api */
-
-
-
-
-
 
 });
